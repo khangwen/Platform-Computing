a7a2ccee51be3814f03db8c8aaf0cbd485a461a7
Alan Nguyen <a_wen197@yahoo.com>
2024-03-11 15:20:43 -0700

    setup p3

--- Assignment_2_User_Interactions/my-app/src/App.js
+++ Assignment_2_User_Interactions/my-app/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div class="maincontainer">
+      <header>
+        <h1>about.</h1>
       </header>
+
+      <div class="intro">
+        <p>I'm a California based Computer Science major, currently studying at CSUSB. I expect to graduate in May
+          2024.
+        </p>
+      </div>
+
+      <div class="education">
+        <h2>education.</h2>
+        <p>Currently, I've taken many courses including: Data Structures and Algorithms, Algorithm Analysis, Web
+          Application Development, Software
+          Engineering, Database Systems, Differential Equations and Linear Algebra, Calculus III, Artificial
+          Intelligence, Operating Systems, Parallel Algorithms, Computer Architecture.</p>
+      </div>
+
+      <div id="skills">
+        <h2>skills.</h2>
+        <p>My skills include: JavaScript, Python, TypeScript, C++, C, Java, SQL. I've worked with tools and
+          technologies
+          such as Node.js, React.js, Express.js, Next.js, Git / GitHub, VSCode, Azure SQL Database</p>
+      </div>
+
+      <div class="contact">
+        <h2>contact.</h2>
+        <p>You can contact me at: khangwen197@gmail.com ● linkedin.com/in/alannguyen197</p>
+      </div>
+
+      <div class="github">
+        <h2>github.</h2>
+        <p>My GitHub is:</p>
+        <a href="https://github.com/khangwen">khangwen</a>
+      </div>
+
+      <img src="/scenery.jpg" alt="scenery" class="scenery" />
     </div>
   );
 }
